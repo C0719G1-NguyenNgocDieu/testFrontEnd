da1e7b60352c64ce2354a254cb57e0487a1fa123
nnd <nndieu88@gmail.com>
2019-12-12 17:03:07 +0700

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import {RichtextService} from './richtext.service';
 })
 export class AppComponent {
   public insertImageSettings = {
-    saveUrl : `http://localhost:8080/upload`
+    saveUrl : `http://localhost:8080/upload`,
+    path: `http://localhost:8080/downloadFile/`
   };
   onSubmit(form: NgForm): void {
     alert(form.value.name);
